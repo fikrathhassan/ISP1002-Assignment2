c6c926dfdfa16326314c172474d4187c984dede4
Fikrath Hassan <fikrathhassan@users.noreply.github.com>
2022-02-18 22:27:07 +0530

    referenced variables with ui elements

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var firstValue: Float = 0
+    var secondValue: Float = 0
+    var result: Float = 0
+    
+    @IBOutlet weak var firstField: UITextField!
+    @IBOutlet weak var secondField: UITextField!
+    @IBOutlet weak var operatorField: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func btnClear() {
+        
+    }
+    @IBAction func btnResult() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
