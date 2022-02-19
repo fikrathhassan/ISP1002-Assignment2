5b62ac0d1bde101b94b8bc118b7a6ea61b03de0f
Fikrath Hassan <fikrathhassan@users.noreply.github.com>
2022-02-19 14:06:56 +0530

    added basic functionalities

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -18,16 +18,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var operatorField: UISegmentedControl!
     @IBOutlet weak var resultLabel: UILabel!
     
+    // This button click clear all the textfileds, input and result values
     @IBAction func btnClear() {
-        
+        firstValue = 0
+        secondValue = 0
+        result = 0
+        firstField.text = "0"
+        secondField.text = "0"
+        operatorField.selectedSegmentIndex = 0
+        resultLabel.text = ""
     }
+    
+    // This button click shows reult on resultLabel based on the operator selected
     @IBAction func btnResult() {
-        
+        if (firstField.text != "" && secondField.text != "") {
+            firstValue = Float(firstField.text!)!
+            secondValue = Float(secondField.text!)!
+            switch operatorField.selectedSegmentIndex {
+            case 0:
+                // addition
+                result = firstValue+secondValue
+            case 1:
+                // subtraction
+                result = firstValue-secondValue
+            case 2:
+                // multiplication
+                result = firstValue*secondValue
+            case 3:
+                // division
+                result = firstValue/secondValue
+            default:
+                print("No cases detected!")
+            }
+            resultLabel.text = String(result)
+        } else {
+            print("Please enter values in textfields!")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // hides keyboard when touched outside textfiled, if shown
+        view.addGestureRecognizer(UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing(_:))))
     }
 
 }
